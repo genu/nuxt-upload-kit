f9c41f741ff60bfb1f1732eccb94118cdbcfbcaa
Eugen Istoc <eugenistoc@gmail.com>
2026-01-14 13:28:15 -0500

    chore: exclude FFmpeg packages from Vite's dependency optimization

--- src/module.ts
+++ src/module.ts
@@ -23,6 +23,14 @@ export default defineNuxtModule<ModuleOptions>({
   setup(options, _nuxt) {
     const resolver = createResolver(import.meta.url)
 
+    // Exclude FFmpeg packages from Vite's dependency optimization
+    // @see https://github.com/ffmpegwasm/ffmpeg.wasm/issues/532
+    // FFmpeg uses Web Workers that don't work correctly when pre-bundled by Vite
+    _nuxt.options.vite = _nuxt.options.vite ?? {}
+    _nuxt.options.vite.optimizeDeps = _nuxt.options.vite.optimizeDeps ?? {}
+    _nuxt.options.vite.optimizeDeps.exclude = _nuxt.options.vite.optimizeDeps.exclude ?? []
+    _nuxt.options.vite.optimizeDeps.exclude.push("@ffmpeg/ffmpeg", "@ffmpeg/util")
+
     if (options.autoImport) {
       // Auto-import useUploadKit composable
       addImports([
